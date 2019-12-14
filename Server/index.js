d2ee32f45c9dc2ba852a0c92ca417a3fe9bc2257
Denis Peshekhonov <denis@peshekhonov.ru>
2019-12-14 05:58:53 +0300

    organize files, add noise

--- Server/index.js
+++ Server/index.js
@@ -1,69 +1,12 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
+const utils = require('./utils');
+const constants = require('./contants');
 
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// constants
-const colorCodes = [
-    [['аквамарин'], '7fffd4'],
-    [['алый', 'алого', 'аленьк', 'алую', 'алой', 'алая', 'алым'], 'ff2400'],
-    [['аметист'], '9966cc'],
-    [['антрацит'], '464451'],
-    [['бежев'], 'f5f5dc'],
-    [['белый', 'белого', 'белую', 'белая', 'белой', 'белым'], 'ffffff'],
-    [['бирюзов'], '30d5c8'],
-    [['бордо'], '9b2d30'],
-    [['бронзов'], 'cd7f32'],
-    [['бурый', 'бурого', 'бурую', 'бурая', 'бурой', 'бурым'], '45161c'],
-    [['голубой'], '42aaff'],
-    [['гранатов'], 'f34723'],
-    [['желт'], 'ffff00'],
-    [['зелен', 'зелён'], '008000'],
-    [['изумруд'], '009b77'],
-    [['индиго'], '4b0082'],
-    [['коричнев'], '964b00'],
-    [['красн'], 'ff0000'],
-    [['лайм'], '00ff00'],
-    [['лилов'], 'db7093'],
-    [['лимон'], 'fde910'],
-    [['малахит'], '0bda51'],
-    [['малинов'], 'dc143c'],
-    [['мандарин'], 'ff8800'],
-    [['небесн'], '7fc7ff'],
-    [['нефрит'], '00a86b'],
-    [['оливков'], '808000'],
-    [['оранж'], 'ffa500'],
-    [['охра', 'охры'], 'cc7722'],
-    [['персик'], 'ffe5b4'],
-    [['песочн'], 'fcdd76'],
-    [['пурпурн'], '800080'],
-    [['розов'], 'ffc0cb'],
-    [['рыжий', 'рыжего', 'рыжая', 'рыжей', 'рыжую', 'рыжим'], 'd77d31'],
-    [['салат'], '99ff99'],
-    [['сапфир'], '082567'],
-    [['серебр'], 'c0c0c0'],
-    [['синий', 'синего', 'синяя', 'синюю', 'синей', 'синим'], '0000ff'],
-    [['сиренев'], 'c8a2c8'],
-    [['скарлет'], 'fc2847'],
-    [['сливов'], '660066'],
-    [['терракот'], 'cc4e5c'],
-    [['тициан'], 'd53e07'],
-    [['томат'], 'ff6347'],
-    [['трав'], '5da130'],
-    [['ультрамарин'], '120a8f'],
-    [['фиалк'], 'ea8df7'],
-    [['фиолетов'], '8b00ff'],
-    [['хаки'], '806b2a'],
-    [['шафран'], 'f4c430'],
-    [['шоколад'], 'd2691e'],
-    [['янтар'], 'ffbf00'],
-];
-
-const colorsNum = 24;
-const ledsNum = 130;
 
 // main params
 let code = 0;
@@ -72,30 +15,23 @@ let partSize = 10;
 let gradient = true;
 let random = false;
 let rainbow = false;
-let colors = Array(colorsNum).fill('ffffff');
+let noise = false;
+let colors = Array(constants.colorsNum).fill('ffffff');
 
 function setColors(_colors) {
-    for (let i = 0; i < colorsNum; i++) {
-        colors[i] = _colors[i % _colors.length];
+    for (let i = 0; i < constants.colorsNum; i++) {
+        let color = _colors[i % _colors.length];
+        if (noise && color !== '000000') {
+            color = utils.randomizeColor(color);
+        }
+        colors[i] = color;
     }
-}
-
-function setMode(_slowness, _partSize, _gradient, _rainbow, _random, _colors) {
-    slowness = Number.parseInt(setNew(slowness, _slowness));
-    partSize = Number.parseInt(setNew(partSize, _partSize));
-    gradient = !!setNew(gradient, _gradient);
-    rainbow = !!setNew(rainbow, _rainbow);
-    random = !!setNew(random, _random);
-    if (_colors !== null && _colors !== undefined) {
-        setColors(_colors.split(','));
-    }
-    code = (code + 1) % 10;
 }
 
 function getMode() {
     let result = code.toString();
-    result += n(slowness);
-    result += n(partSize);
+    result += utils.n(slowness);
+    result += utils.n(partSize);
     result += gradient ? '1' : '0';
     result += random ? '1' : '0';
     if (rainbow) {
@@ -109,20 +45,34 @@ function getMode() {
 // handle request
 app.post('/changeColor', (req, res) => {
     const command = (req.body.value1 || '').toLowerCase().split(' ');
+
+    // gradient
+    gradient = utils.hasKeywords(command, constants.kwGradient);
+    
+    // random
+    random = utils.hasKeywords(command, constants.kwRandom);
+
+    // noise
+    noise = random || utils.hasKeywords(command, constants.kwNoise);
     
     // colors
-    if (hasKeywords(command, ['радуг', 'радужн', 'разноцветн'])) {
+    if (utils.hasKeywords(command, constants.kwRainbow)) {
         rainbow = true;
     } else {
         rainbow = false;
+        // fill colors array
         const newColors = [];
         for (colorCode of colorCodes) {
-            if (hasKeywords(command, colorCode[0])) {
+            if (utils.hasKeywords(command, colorCode[0])) {
                 newColors.push(colorCode[1]);
             }
         }
+        // shuffle if random
+        if (random) {
+            utils.shuffle(newColors);
+        }
         // special case for black, alternate black with colors
-        if (hasKeywords(command, ['черн', 'чёрн'])) {
+        if (utils.hasKeywords(command, constants.kwBlack)) {
             let len = newColors.length;
             while (len > 0) {
                 newColors.splice(len--, 0, String('000000'));
@@ -132,33 +82,29 @@ app.post('/changeColor', (req, res) => {
             setColors(newColors);
         }
     }
-
+    
     // slowness
-    if (hasKeywords(command, ['быстр', 'резк'])) {
+    if (utils.hasKeywords(command, constants.kwFast)) {
         slowness = 1;
-    } else if (hasKeywords(command, ['медлен', 'плавн'])) {
+    } else if (utils.hasKeywords(command, constants.kwSlow)) {
         slowness = 10;
     } else {
         slowness = 5;
     }
-
+    
     // parts size
     for (token of command) {
         const number = Number.parseInt(token);
         if (!Number.isNaN(number)) {
-            partSize = Math.min(Math.max(Math.round(ledsNum / number), 1), ledsNum);
+            const pSize = Math.round(constants.ledsNum / number);
+            partSize = utils.limit(pSize, 1, constants.ledsNum);
+            break;
         }
     }
-    if (hasKeywords(command, ['полностью', 'всю', 'весь', 'вся', 'всей', 'целиком', 'целой', 'целую', 'целый'])) {
+    if (utils.hasKeywords(command, constants.kwFull)) {
         partSize = 0;
     }
-
-    // gradient
-    gradient = hasKeywords(command, ['градиент', 'перелив']);
-
-    // random
-    random = hasKeywords(command, ['случайн', 'перемеш', 'вперемеш', 'хаотич', 'рандом', 'вразнобой', 'разнобой', 'беспоряд']);
-
+    
     code = (code + 1) % 10;
     console.log('new mode set: ' + getMode());
     res.send('');
@@ -166,47 +112,21 @@ app.post('/changeColor', (req, res) => {
 
 // set mode manual
 app.get('/setMode', (req, res) => {
-    setMode(
-        req.query.slowness,
-        req.query.partSize,
-        req.query.gradient,
-        req.query.rainbow,
-        req.query.random,
-        req.query.colors
-    );
+    slowness = Number.parseInt(utils.setNew(slowness, req.query.slowness));
+    partSize = Number.parseInt(utils.setNew(partSize, req.query.partSize));
+    gradient = !!utils.setNew(gradient, req.query.gradient);
+    rainbow = !!utils.setNew(rainbow, req.query.rainbow);
+    random = !!utils.setNew(random, req.query.random);
+    if (req.query.colors !== null && req.query.colors !== undefined) {
+        setColors(req.query.colors.split(','));
+    }
+    code = (code + 1) % 10;
     res.send(getMode());
 });
-
+    
 // get current mode
 app.get('/mode', (req, res) => {
     res.send(getMode());
 });
-
-function n(num) {
-    const s = num.toString();
-    if (s.length < 2) {
-        return `0${s}`;
-    }
-
-    return s;
-}
-
-function setNew(sourceVal, newVal) {
-    if (newVal !== null && newVal !== undefined) {
-        return newVal;
-    }
-    return sourceVal;
-}
-
-function hasKeywords(command, keywords) {
-    for (let keyword of keywords) {
-        for (word of command) {
-            if (word.startsWith(keyword)) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
---
+++ Server/utils.js
@@ -0,0 +1,135 @@
+module.exports.n = function(num) {
+    const s = num.toString();
+    if (s.length < 2) {
+        return `0${s}`;
+    }
+    
+    return s;
+}
+
+module.exports.randomInt = function(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+module.exports.setNew = function(sourceVal, newVal) {
+    if (newVal !== null && newVal !== undefined) {
+        return newVal;
+    }
+    return sourceVal;
+}
+
+module.exports.hasKeywords = function(command, keywords) {
+    for (let keyword of keywords) {
+        for (word of command) {
+            if (word.startsWith(keyword)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+module.exports.limit = function(val, min, max) {
+    return Math.min(Math.max(val, min), max);
+}
+
+module.exports.hexToHSL = function(H) {
+    // convert hex to RGB first
+    let r = 0, g = 0, b = 0;
+    if (H.length == 4) {
+        r = "0x" + H[1] + H[1];
+        g = "0x" + H[2] + H[2];
+        b = "0x" + H[3] + H[3];
+    } else if (H.length == 7) {
+        r = "0x" + H[1] + H[2];
+        g = "0x" + H[3] + H[4];
+        b = "0x" + H[5] + H[6];
+    }
+    // then to HSL
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    let cmin = Math.min(r,g,b),
+    cmax = Math.max(r,g,b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+    
+    if (delta == 0)
+    h = 0;
+    else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+    h = (b - r) / delta + 2;
+    else
+    h = (r - g) / delta + 4;
+    
+    h = Math.round(h * 60);
+    
+    if (h < 0)
+    h += 360;
+    
+    l = (cmax + cmin) / 2;
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+    
+    return [h, s, l];
+}
+
+module.exports.HSLToHex = function(hsl) {
+    let h = hsl[0],
+    s = hsl[1] / 100,
+    l = hsl[2] / 100;
+    
+    if (h >= 360) h %= 360;
+    
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+    m = l - c/2,
+    r = 0,
+    g = 0,
+    b = 0;
+    
+    if (0 <= h && h < 60) {
+        r = c; g = x; b = 0;
+    } else if (60 <= h && h < 120) {
+        r = x; g = c; b = 0;
+    } else if (120 <= h && h < 180) {
+        r = 0; g = c; b = x;
+    } else if (180 <= h && h < 240) {
+        r = 0; g = x; b = c;
+    } else if (240 <= h && h < 300) {
+        r = x; g = 0; b = c;
+    } else if (300 <= h && h < 360) {
+        r = c; g = 0; b = x;
+    }
+    // having obtained RGB, convert channels to hex
+    r = Math.round((r + m) * 255).toString(16);
+    g = Math.round((g + m) * 255).toString(16);
+    b = Math.round((b + m) * 255).toString(16);
+    
+    return this.n(r) + this.n(g) + this.n(b);
+}
+
+module.exports.randomizeColor = function(color) {
+    const hsl = this.hexToHSL(`#${color}`);
+    hsl[0] -= this.randomInt(-10, 10);
+    if (hsl[0] > 360) hsl[0] -= 360;
+    if (hsl[0] < 0) hsl[0] -= -360;
+
+    for (let i = 1; i <= 2; i++) {
+        hsl[i] = this.limit(hsl[i] - this.randomInt(-10, 10), 0, 100);
+    }
+
+    return this.HSLToHex(hsl);
+}
+
+module.exports.shuffle = function(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
